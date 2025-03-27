88ddb70683b3190225a9af61a114872a751720bd
dimpleti <neelmani.tiwari@ohiapp.com>
2025-03-27 14:58:11 +0530

    Support addition of two numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -8,4 +8,7 @@ RSpec.describe StringCalculator do
   it "returns the number itself for a single number" do
     expect(calculator.add("1")).to eq(1)
   end
+  it "returns sum for two numbers" do
+    expect(calculator.add("1,5")).to eq(6)
+  end
 end
